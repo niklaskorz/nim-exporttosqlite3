64c5cb524a35935c3937dbf05ffd416167b04cc8
Niklas Korz <niklas@niklaskorz.de>
2021-06-24 10:15:48 +0200

    Implement all necessary scalar types

--- src/exporttosqlite3.nim
+++ src/exporttosqlite3.nim
@@ -6,18 +6,32 @@ export sqlite3
 var registrationFunctions: seq[proc(db: PSqlite3): int32]
 
 macro exportToSqlite3*(input: untyped): untyped =
+  template paramCstring(identifier, argIndex): untyped =
+    let identifier = value_text(argv[argIndex])
   template paramString(identifier, argIndex): untyped =
     let identifier = $value_text(argv[argIndex])
   template paramInt32(identifier, argIndex): untyped =
     let identifier = value_int(argv[argIndex])
   template paramInt64(identifier, argIndex): untyped =
     let identifier = value_int64(argv[argIndex])
-  template resultString(): untyped =
+  template paramFloat64(identifier, argIndex): untyped =
+    let identifier = value_double(argv[argIndex])
+  template paramBool(identifier, argIndex): untyped =
+    let identifier = bool(value_int(argv[argIndex]))
+
+  template resultCstring(): untyped =
     context.result_text(nimResult, int32(nimResult.len), nil)
+  template resultString(): untyped =
+    let nimResultCstring = cstring(nimResult)
+    context.result_text(nimResultCstring, int32(nimResultCstring.len), nil)
   template resultInt32(): untyped =
     context.result_int(nimResult)
   template resultInt64(): untyped =
     context.result_int64(nimResult)
+  template resultFloat64(): untyped =
+    context.result_double(nimResult)
+  template resultBool(): untyped =
+    context.result_int(int32(nimResult))
 
   assert input.kind == nnkProcDef
   let ident = input[0]
@@ -32,12 +46,18 @@ macro exportToSqlite3*(input: untyped): untyped =
     assert param.kind == nnkIdentDefs
     assert param[0].kind == nnkIdent
     assert param[1].kind == nnkIdent
-    if eqIdent(param[1], "string"):
+    if eqIdent(param[1], "cstring"):
+      wrapperBody.add(getAst(paramCstring(param[0], paramIndex)))
+    elif eqIdent(param[1], "string"):
       wrapperBody.add(getAst(paramString(param[0], paramIndex)))
     elif eqIdent(param[1], "int32"):
       wrapperBody.add(getAst(paramInt32(param[0], paramIndex)))
     elif eqIdent(param[1], "int64"):
       wrapperBody.add(getAst(paramInt64(param[0], paramIndex)))
+    elif eqIdent(param[1], "float64") or eqIdent(param[1], "float"):
+      wrapperBody.add(getAst(paramFloat64(param[0], paramIndex)))
+    elif eqIdent(param[1], "bool"):
+      wrapperBody.add(getAst(paramBool(param[0], paramIndex)))
     else:
       error "parameter type `" & strVal(param[1]) & "` cannot be exported to sqlite3", param[1]
     callArgs.add(param[0])
@@ -48,12 +68,18 @@ macro exportToSqlite3*(input: untyped): untyped =
 
   let returnType = params[0]
   assert returnType.kind == nnkIdent
-  if eqIdent(returnType, "string"):
+  if eqIdent(returntype, "cstring"):
+    wrapperBody.add(getAst(resultCstring()))
+  elif eqIdent(returnType, "string"):
     wrapperBody.add(getAst(resultString()))
   elif eqIdent(returnType, "int32"):
     wrapperBody.add(getAst(resultInt32()))
   elif eqIdent(returnType, "int64"):
     wrapperBody.add(getAst(resultInt64()))
+  elif eqIdent(returnType, "float64") or eqIdent(returnType, "float"):
+    wrapperBody.add(getAst(resultFloat64()))
+  elif eqIdent(returnType, "bool"):
+    wrapperBody.add(getAst(resultBool()))
   else:
     error "return type `" & strVal(returnType) & "` cannot be exported to sqlite3", returnType
 
--- tests/test.nim
+++ tests/test.nim
@@ -1,12 +1,17 @@
 import unittest
 import sugar
+import math
 import db_sqlite
 import exporttosqlite3
 
 proc myNimFunction(greeting: string, name: string, age: int32): string {.exportToSqlite3.} =
   greeting & " " & name & " (age " & $age & ")"
 
-test "can run exported functions in sqlite3":
+proc isOld(age: int32): bool {.exportToSqlite3.} = age > 20
+
+proc squareRoot(value: float): float {.exportToSqlite3.} = sqrt(value)
+
+test "can run exported functions in UPDATE":
   let db = open("test.db", "", "", "")
   defer:
     db_sqlite.close(db)
@@ -16,11 +21,24 @@ test "can run exported functions in sqlite3":
   db.exec(sql"INSERT INTO students (name, age) VALUES (?, ?), (?, ?)",
       "Peter Parker", 23, "John Good", 19)
   db.exec(sql"UPDATE students SET name = myNimFunction('Hello', name, age)")
-  var columns: DbColumns
   var names = collect(newSeq):
-    for row in db.instantRows(columns, sql"SELECT name FROM students"):
+    for row in db.instantRows(sql"SELECT name FROM students"):
       row[0]
   check names == @[
     "Hello Peter Parker (age 23)",
     "Hello John Good (age 19)"
   ]
+
+test "can use exported functions in SELECT":
+  let db = open("test.db", "", "", "")
+  defer:
+    db_sqlite.close(db)
+  db.registerFunctions()
+  db.exec(sql"DROP TABLE IF EXISTS students")
+  db.exec(sql"CREATE TABLE students (name TEXT, age INT)")
+  db.exec(sql"INSERT INTO students (name, age) VALUES (?, ?), (?, ?)",
+      "Peter Parker", 23, "John Good", 19)
+  var squareRoots = collect(newSeq):
+    for row in db.instantRows(sql"SELECT squareRoot(age) FROM students WHERE isOld(age)"):
+      row[0]
+  check squareRoots == @["4.79583152331272"]
